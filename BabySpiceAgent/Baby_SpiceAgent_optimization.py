f601eada3e03e5dfa14eb218615c21ef81406a91
Davide_Milillo <davide.milillo@uniroma3.it>
2025-12-19 18:01:34 +0100

    add again netlist Iit needs it)

--- BabySpiceAgent/Baby_SpiceAgent_optimization.py
+++ BabySpiceAgent/Baby_SpiceAgent_optimization.py
@@ -80,14 +80,13 @@ with open(os.path.join(output_folder, "RC_circuit_sim.net"), 'r') as f:
 # Construct Prompt
 prompt = f"""
 We are analyzing an RC circuit simulated in LTSpice.
-The circuit is excited with a 
-pulse voltage source of 5 V amplitude, 1μs rise and fall time, 5ms pulse width, and 10ms period.
+Here is the net list:
+{netlist_content}
 
 Here is the results as voltage on the capacitor C, Vn2:
 {data_str}
 
-Carefully analyze the RC circuit behavior and try to esitmate the tau constant, tau_old, 
-of the circuit considering the voltage response on the capacitor.
+Esitmate the tau constant, tau_old, of the circuit .
 Change the R and/or C values to obtain a time constant of 2 milliseconds.
 
 Return ONLY a JSON object with the old time constant, tau_old, and new R and C values only.
