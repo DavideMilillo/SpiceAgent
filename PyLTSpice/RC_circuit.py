7c51b7f44252bf4fcd44693a8fe22e32a7f34956
Davide_Milillo <davide.milillo@uniroma3.it>
2025-12-19 15:51:40 +0100

    RC circuit LTSpice

---
+++ PyLTSpice/RC_circuit.py
@@ -0,0 +1,22 @@
+#Test PyLTSpice RC Circuit Example
+import PyLTSpice
+from PyLTSpice import SimRunner
+from PyLTSpice import RawRead
+import numpy as np
+
+
+#open the circuit
+
+#read the circuit file
+
+
+#print the circuit file
+
+
+#modify the values
+
+
+#run a simulation
+
+
+#show the results
