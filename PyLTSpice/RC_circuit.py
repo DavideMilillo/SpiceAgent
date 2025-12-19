66baab552cfbe05d1ae8ed3cf6ad7a19998c5e53
Davide_Milillo <davide.milillo@uniroma3.it>
2025-12-19 15:58:12 +0100

    basic istructions RC circuit

--- PyLTSpice/RC_circuit.py
+++ PyLTSpice/RC_circuit.py
@@ -1,11 +1,15 @@
 #Test PyLTSpice RC Circuit Example
 import PyLTSpice
 from PyLTSpice import SimRunner
+from PyLTSpice import SpiceEditor
 from PyLTSpice import RawRead
 import numpy as np
 
 
 #open the circuit
+#how can I convert the circuit file to a netlist object?
+netlist = SpiceEditor('RC_circuit.net') 
+
 
 #read the circuit file
 
@@ -14,9 +18,19 @@ import numpy as np
 
 
 #modify the values
+netlist.set_component_value('R1', '10k') #R value
+netlist.set_component_value('C1', '100n') #C value
 
+netlist.remove_instructions(".tran")   #remove any previous instructions
+
+netlist.add_instructions(".tran", "10m") #tran analysis for 10ms
 
 #run a simulation
+n_sim = 100 #number of simulations
+
+#ciclo di simulazione
+runner = SimRunner(output_filename="RC_circuit_sim.raw", netlist=netlist)
+runner.run(netlist)
 
 
 #show the results
