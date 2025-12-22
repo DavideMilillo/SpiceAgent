537d6eb2c228bb8c4686dc8db27a15174510ccc4
Davide_Milillo <davide.milillo@uniroma3.it>
2025-12-22 12:12:09 +0100

    created PowerAgent_v1

---
+++ PowerAgent/PowerAgent_V1.py
@@ -0,0 +1,11 @@
+"""First version of
+SpiceAgent - PowerAgent
+An AI-agent for optimization of power electronics circuits
+
+The first version focuses on
+finding the best parameters for a buck converter to achieve
+specifications given.
+The circuit is considered as fixed and in steady state.
+
+Author: Davide Milillo
+"""
